f4a34d4b9ff1d662e71c61e477f45fdf54a1636d
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-03 17:13:20 +0300

    (ch2-string-formatting-and-comment-in-code-> docs: Add KDoc comments to main function and print escape sequences
                                                 - Add KDoc comments to the main function, including author, version, and description
                                                 - Document the usage of various escape sequences in println statements

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * These @symbols are collectively known as KDoc. Some of these are shown below:
+ * The *main* Function used to start
+ * this simple Hello World application.
+ * @author Ahmed Samir -> the author of the code
+ * @since 1.0 -> versions
+ * @see Main -> cross-references or where is your point that you will describe and document
+ * @params functions -> symbol used to describe parameters to functions
+ */
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("\tHello — Inserts tab")
+    println("\bHello — Inserts backspace")
+    println("\nHello — Inserts newline")
+    println("\rHello — Inserts carriage return")
+    println("\'Hello — Inserts single quote character")
+    println("\"Hello — Inserts double quote character")
+    println("\\Hello — Inserts backslash")
+    println("\$Hello — Inserts dollar character")
 }
+
