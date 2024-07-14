fb0b1a96d740882304504bd88eb3496c60c098c4
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-14 19:48:44 +0300

    (ch2-A-First-Kotlin-Program/Numeric-Conversions) -> Added examples and comments for numeric type conversions in Kotlin
                                                        - Documented Kotlin's explicit numeric type conversion methods.
                                                        - Demonstrated conversions using examples for each method, covering conversions between Int, Double, Long, Float, Byte, Short, and Char.
                                                        - Included a comprehensive comment block outlining Kotlin's approach to numeric type conversions.
                                                        - Structured code with clear examples and explanations for better understanding.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Unlike many languages Kotlin does not automatically convert from one numeric type
+ * to another. Instead explicit conversion operators/methods must be used. All numeric
+ * types support conversions to other types using the following methods:
+ * • toByte(): Byte
+ * • toShort(): Short
+ * • toInt(): Int
+ * • toLong(): Long or use the latter L after an integer literal, e.g,. 34L
+ * • toFloat(): Float or use the letter F after ta numeric literal e.g. 34.5F
+ * • toDouble(): Double
+ * • toChar(): Char
+ */
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // toByte(): Byte
+    val intToByte: Byte = 10.toByte()
+    println("Converted Int to Byte: $intToByte")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    // toShort(): Short
+    val intToShort: Short = 100.toShort()
+    println("Converted Int to Short: $intToShort")
+
+    // toInt(): Int
+    val doubleToInt: Int = 3.14.toInt()
+    println("Converted Double to Int: $doubleToInt")
+
+    // toLong(): Long
+    val intToLong: Long = 10000000000L
+    println("Converted Int to Long: $intToLong")
+
+    // toFloat(): Float
+    val doubleToFloat: Float = 3.14.toFloat()
+    println("Converted Double to Float: $doubleToFloat")
+
+    // toDouble(): Double
+    val floatToDouble: Double = 3.14F.toDouble()
+    println("Converted Float to Double: $floatToDouble")
+
+    // toChar(): Char
+    val intToChar: Char = 65.toChar()
+    println("Converted Int to Char: $intToChar")
 }
