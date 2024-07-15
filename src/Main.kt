2eb3446aa9c8614e3f0c8d437ff272b16cfcb41b
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-15 14:39:50 +0300

    (ch3-Flow-of-Control/6_Loop-Control-Statements) -> Add examples of 'break' and 'continue' statements in Kotlin loops
    - Break Loop Statement:
      - First Example: Demonstrates breaking out of a loop when a condition is met.
      - Second Example: Shows how to use labeled breaks to exit nested loops.
    - Continue Loop Statement:
      - First Example: Demonstrates continuing to the next iteration of a loop when a condition is met.
      - Second Example: Uses labeled continue to skip iterations in nested loops.
    - When with Continue and Break:
      - Combines 'when' expression with 'continue' and 'break' statements to control loop flow.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    //region Break Loop Statement
+    println("(Break Loop Statement)")
+    println("First Example:")
+    val y = 10
+    val x = 5
+    for (i in 0 until y) {
+        if (i == x) {
+            break
+        }
+        print("i: $i, ")
     }
+    println("Done")
+    println("\n")
+
+
+    println("Second Example:")
+    loop@ for (i in 0 until 4) {
+        for (j in 0 until 2) {
+            if (i == 3) {
+                break@loop
+            }
+            println("i=$i - j=$j, ")
+        }
+    }
+    println("-------------------------------------------------- \n\n")
+    //endregion
+
+    //region Continue Loop Statement
+    println("(Continue Loop Statement)")
+    println("First Example:")
+    for (i in 0 until 10) {
+        print("i=$i ")
+        if ((i % 2) == 1) {
+            continue
+        }
+        println("hey its an even number")
+        println("we love even numbers")
+    }
+    println("Done")
+    println("\n")
+
+
+    println("Second Example:")
+    outer@ for (i in 1..4) {
+        println("i = $i")
+        for (j in 1..4) {
+            val result = i + j
+            if (result == 5) continue@outer
+            println("\t$i + $j = $result ")
+        }
+    }
+    println("-------------------------------------------------- \n\n")
+    //endregion
+
+    //region When with Continue and Break
+    println("(Continue Loop Statement)")
+    for (i in 0..10) {
+        when (i) {
+            2 -> continue
+            4 -> break
+            else -> println(i)
+        }
+    }
+    println("Done")
+    println("-------------------------------------------------- \n\n")
+    //endregion
+
 }
