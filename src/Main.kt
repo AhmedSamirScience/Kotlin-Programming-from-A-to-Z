9afb93d77953af390a52e460c1ee00adad5a6f8f
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-15 13:55:51 +0300

    (ch3-Flow-of-Control/For-Loop) -> Add examples of 'for' loops with various range and step conditions in Kotlin
    - Print out values in a range: Demonstrates a basic 'for' loop iterating from 0 to 9.
    - Print out values in a range with an increment of 2: Shows how to use a 'for' loop with a step increment.
    - Print out values in a range to but not including 9: Uses the 'until' keyword to exclude the upper bound in the loop.
    - Print out values in a range to but not including 9 with step 2: Combines 'until' with a step increment.
    - Iterate down from one value to a lower value: Demonstrates a descending 'for' loop with the 'downTo' keyword, including squaring the values.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    //region Print out values in a range
+    println("Print out values in a range")
+    for (i in 0..9) {
+        print("$i, ")
     }
+    println()
+    println("Done")
+    println("\n")
+    //endregion
+
+    //region Print out values in a range with an increment of 2
+    // Now use values in a range but increment by 2
+    println("Print out values in a range with an increment of 2")
+    for (i in 0..9 step 2) {
+        print("$i, ")
+    }
+    println()
+    println("Done")
+    println("\n")
+    //endregion
+
+    //region Print out values in a range to but not including 9
+    println("Print out values in a range to but not including 9")
+    for (x in 0 until 9)
+        print("$x, ")
+    println()
+    println("Done")
+    println("\n")
+    //endregion
+
+    //region Print out values in a range to but not including 9 with step 2
+    println("Print out values in a range to but not including 9 with step 2")
+    for (x in 0 until 9 step 2) {
+        print("$x, ")
+    }
+    println()
+    println("Done")
+    println("\n")
+    //endregion
+
+    //region Iterate down from one value to a lower value
+    println("Iterate down from one value to a lower value")
+    for (i in 5 downTo 0) {
+        print("$i * $i = ${i*i}; ")
+    }
+    println()
+    println("Done")
+    println("\n")
+    //endregion
+
 }
