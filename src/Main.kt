0a6059fffa3c3a6bfbb4b9c4dd385db1c349165b
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-26 18:11:14 +0300

    (ch4-Functions-i-kotlin/4-Anonymous-Functions) -> docs: Add explanation for anonymous functions in Kotlin
    - Document the concept of anonymous functions in Kotlin, including their syntax and use cases.
    - Illustrate how anonymous functions can be defined and used, with a specific example using `val func`.
    - Explain the role of the return type in anonymous functions and the fact that they can only be invoked where they are declared unless stored in a variable.
    
    This commit enhances documentation on anonymous functions in Kotlin, providing clarity on their syntax, utility, and behavior.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Kotlin supports anonymous functions, which are unnamed functions defined as:
+ *
+ * fun(parameter list): return-type { func body }
+ *
+ * The return-type is optional but required if the function returns a value.
+ * Anonymous functions are useful for defining behavior on the fly without adding extra names to your program.
+ * They can only be invoked at the point of declaration unless stored in a variable.
+ */
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val func = fun(i: Int): Int { return i + 1 }
+
+    /**
+     * When the val func, along with the round brackets () is used above, it accesses
+     * the anonymous function and executes it. So, this causes the value 5 to be passed into the
+     * anonymous function and to have its value incremented by one.
+     */
+    println(func(5))
+
 }
