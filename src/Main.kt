58e0574b59b2b46aa42ebda4b33e7ef2c8d18074
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-26 16:30:32 +0300

    (ch4-Functions-i-kotlin/0-Default-Parameter-Values) -> feat: Add greeter2 function with default parameter values
    - Implement greeter2() function demonstrating mandatory and optional parameters.
    - Set default value for the 'message' parameter to "Live Long and Prosper".
    - Include examples in main() to illustrate usage of default and overridden parameter values.
    
    This commit showcases how to handle optional function parameters in Kotlin, improving flexibility in function calls.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * The default value comes after the type, preceded by an equals sign =, and must match the parameter type.
+ * If a value is provided, it overrides the default; otherwise, the default is used.
+ */
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    /**
+     * | Parameter | Type     | Mandatory/Optional | Default Value            |
+     * |-----------|----------|--------------------|--------------------------|
+     * | name      | String   | Mandatory          | None                     |
+     * | message   | String   | Optional           | "Live Long and Prosper"  |
+     *
+     * In the first example, where only one argument was provided, the default message was used. In the second example, a provided message was used instead of the default.
+     * In `greeter2()`, the term "mandatory" is used for the `name` parameter, and "optional" is used for the `message` parameter, which has a default value.
+     */
+    greeter2("Theeban")
+    greeter2("Jasmine", "Have a Nice Day!")
+
+}
+
+fun greeter2(name: String, message: String = "Live Long and Proper") {
+    println("Welcome $name - $message")
 }
