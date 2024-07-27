92f9baae141e1ba0cbade84d5a51aacd243b0ce2
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-07-27 21:20:21 +0300

    (ch6-Higher-Order-Functions//1-Box-Operator) -> docs: Demonstrate function references using the :: operator
    - Added examples showing how to use the box operator `::` to obtain a reference to a named function in Kotlin.
    - Illustrated the difference between directly invoking a function and referencing it using `::`.
    - Provided a detailed explanation of how `::increase` is used to obtain a callable reference to the `increase` function.
    - Included output examples to demonstrate the result of calling the function directly versus through its reference.
    
    This commit helps in understanding function references and the `::` operator in Kotlin.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+
+    /**
+     * The box operator (::) can be used to obtain a reference to a named function.
+     * By default, when you access a named function, it must be invoked.
+     * It is not possible to simply reference the function, as shown below:
+     */
+    println(increase(5))
+    /**
+     * Attempting this will result in a compilation error: Function invocation 'increase(...)' expected.
+     */
+    //val inc = increase
+
+
+    /**
+     * However, using the box operator (::), you can reference a named function without invoking it. The modified program is as follows:
+     */
+    val inc = ::increase // obtain reference to function
+    println(inc)
+    println(inc(5))
+    /**
+     * In this program, we obtain a callable reference to the increase() function and store it in val inc.
+     * This means that both increase and inc reference the same function.
+     * Calling either increase(5) or inc(5) will execute the same block of code and produce the same result, 6.
+     *
+     *
+     * The output from this program is:
+     * 6
+     * fun increase(kotlin.Int): kotlin.Int
+     * 6
+     *
+     *
+     * The reference held in inc points to the increase function, which takes an Int and returns an Int.
+     * Thus, the type of val inc is (Int) -> Int.
+     *
+     */
 }
+
+fun increase(i: Int) = i + 1
