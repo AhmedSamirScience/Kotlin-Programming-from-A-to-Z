1daef7b6b1636b363584318bb1e2a04f348745b6
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-04 01:34:14 +0300

    (ch8-Kotlin-Classes/3-Providing-KDoc-For-Classes) -> docs: Add KDoc examples for class documentation
    - Introduce KDoc usage for documenting classes, properties, and member functions in Kotlin
    - Provide example documentation for the Person class, including description, properties, constructor, author, and version
    - Demonstrate how KDoc comments are structured with summary and block tags
    
    This commit adds examples demonstrating how to use KDoc for clear and structured class documentation in Kotlin.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * Providing KDoc
+ * KDoc is used to document classes, properties, and member functions in Kotlin, explaining their purpose and any important points. KDoc comments start with /** and end with */, with each line optionally beginning with an asterisk.
+ *
+ * By convention, the first paragraph is a summary description, and the following text is a detailed description. IntelliJ can use this summary to provide a popup with more information.
+ *
+ * Block tags start on a new line with the @ character. Here's an example for the Person class:
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+/**
+ * This is a simple class representing information
+ * about a person.
+ *
+ * @property name the name of this person.
+ * @property age the age of this person.
+ * @constructor Creates a new person.
+ *
+ * @author John Hunt
+ * @since 1.0
+ */
+class Person(val name: String = "", var age: Int = 0) {
+    override fun toString() = "Person($name, $age)"
+}
+
+/**
+ * This KDoc provides a clear and structured way to document the Person class and its properties.
+ */
+
+fun main() {
+
 }
