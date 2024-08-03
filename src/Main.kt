fb973cbe96a79382347e0cc5b727bba483cf89fb
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-04 01:50:09 +0300

    (ch8-Kotlin-Classes/4-Equality-Between-Instances) -> docs: Demonstrate equality between instances in Kotlin
    - Provide an example of equality operators in Kotlin: == (structural equality) and === (referential equality)
    - Include an overridden equals() function in the Person class to show how structural equality is determined
    - Demonstrate usage of ==, ===, !=, and !== operators with examples and expected output
    
    This commit adds documentation and examples to illustrate how to compare instances in Kotlin using different equality operators.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+/**
+ * Equality Between Instances
+ * In Kotlin, equality between instances is represented by the operators == and === (and their not versions != and !==).
+ * They have different meanings:
+ *
+ * == represents structural or value equality. For example, two instances of the type Person are considered equal if their data is the same (e.g., "John" aged 32 is equal to another "John" aged 32).
+ * != is the inverse of ==.
+ * === represents referential equality, meaning the instances are literally the same (i.e., the same object in memory).
+ * !== is the inverse of ===.
+ * Here's an example using the Person class that overrides the equals() function:
+ */
+class Person(val name: String, val age: Int) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            null -> false
+            is Person -> (other.name == name) && (other.age == age)
+            else -> false
+        }
     }
+}
+
+fun main() {
+
+    val p1 = Person("John", 21)
+    val p2 = Person("John", 21)
+    val p3 = p1
+
+    // checks for reference equality
+    println("p1 === p2: ${p1 === p2}")
+    // checks for structural equality
+    println("p1 == p2: ${p1 == p2}")
+    // again structural equality
+    println("p1.equals(p2): ${p1.equals(p2)}")
+    println("p1 !== p2: ${p1 !== p2}")
+    println("p1 === p3: ${p1 === p3}")
+    println("p1 !== p3: ${p1 !== p3}")
+
+    /**
+     * Output
+     * p1 === p2: false
+     * p1 == p2: true
+     * p1.equals(p2): true
+     * p1 !== p2: true
+     * p1 === p3: true
+     * p1 !== p3: false
+     *
+     */
 }
