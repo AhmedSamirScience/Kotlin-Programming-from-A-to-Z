d317c8d1bf67e9626279fa547d467fa121ab7bcd
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-04 01:20:07 +0300

    (ch8-Kotlin-Classes/2_Referencing-Member-Functions) -> docs: Add example of callable references in Kotlin
    - Document how to create callable references using the box operator (::)
    - Provide examples for referencing member functions on a class and specific instance
    - Include usage of callable references for `String::toInt` and a bound callable reference `"4"::toInt`
    
    This commit adds documentation demonstrating the usage of callable references in Kotlin, including examples and explanations.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * Referencing Member Functions
+ * You can create a callable reference to a member function using the box operator (::).
+ * This allows you to reference a member function at one point in your program and invoke it at another.
+ * The callable reference can be for a class (e.g., String) or a specific instance (e.g., the string "42").
+ *
+ * Here’s an example of a callable reference to the toInt() member function of the String class:
+ * // A callable reference to a method on class String
+ * val func3: (String) -> Int = String::toInt
+ *
+ * You can use this by calling func3() and passing in a string:
+ * println("func3('3'): ${func3("3")}")
+ *
+ * For a specific string, you can use a bound callable reference:
+ * // A bound callable reference
+ * val func4: () -> Int = "4"::toInt
+ *
+ * Invoke this by calling the function without any parameters:
+ * println("func4(): ${func4()}")
+ *
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+
+
 }
