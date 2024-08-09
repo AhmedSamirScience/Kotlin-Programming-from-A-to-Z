e092afbe57da48f0f4c609b6cc9fcdafbc18292b
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-10 00:31:19 +0300

    (ch8-Kotlin-Classes/1.1-Defining-Default-Constructor-Parameters) -> docs: Add KDoc documentation for default constructor parameters in Kotlin
    - Explain how to provide default values for constructor parameters
    - Include example with the Person2 class demonstrating default values
    - Illustrate different ways to instantiate the class with and without providing arguments
    
    This documentation clarifies how default parameter values work in Kotlin constructors, highlighting their use in simplifying object creation and enhancing Java interoperability.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * In Kotlin, you can provide default values for constructor parameters, making them optional.
+ * To do this, add an assignment operator (=) followed by the default value in the parameter list.
+ * Here’s an example with the Person2 class:
+ */
+class Person2(val name: String = "", val age: Int = 0)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+/**
+ * In Person2, both name and age are optional parameters, with name defaulting to "" and age to 0.
+ *
+ * If all parameters have default values, the compiler generates a zero-parameter constructor, which is useful for Java interoperability.
+ *
+ * Given the Person2 class definition, you can create instances in the following ways:
+ */
+fun main() {
+
+    /**
+     * p1 is an instance of Person2 with name as "John" and age as 36.
+     * p2 is an instance with name as "Denise" and age using the default value 0.
+     * p3 is an instance with both name and age using their default values, "" and 0, respectively.
+     */
+    val p1 = Person2("John", 36)
+    val p2 = Person2("Denise")
+    val p3 = Person2()
 }
