e1b18f9a8e8319da9876b61b18b25f99ca8e4f69
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-10 00:26:11 +0300

    (ch8-Kotlin-Classes/0-Class-Definitions) -> docs: Add KDoc documentation for class definitions in Kotlin
    - Include general format and example of a class definition
    - Explain key points about class name, properties, constructor, and class body
    - Note on naming conventions for class files
    
    This documentation provides a clear explanation of class definitions in Kotlin, including syntax, components, and best practices for naming and organizing class files.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * Class Definitions in Kotlin
+ *
+ * In Kotlin, a class definition follows this general format:
+ * class NameOfClass constructor(<constructor parameters>) {
+ *     init {
+ *         // Initialization block
+ *     }
+ *     // Member properties
+ *     // Member functions
+ * }
+ * You can mix the order of init blocks, properties, and member functions within a class as needed.
+ *
+ * Here's an example of a class definition:
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+class Person constructor(val name: String, var age: Int) { }
+
+/**
+ * Key Points About This Class Definition:
+ *
+ * Class Name: It defines a public class called Person.
+ *
+ * Properties: The class has two properties, name and age.
+ * name is a public read-only property (val).
+ * age is a public read-write property (var).
+ *
+ * Constructor: The class defines a constructor with two parameters:
+ * name of type String to initialize the name property.
+ * age of type Int to initialize the age property.
+ *
+ * Keyword constructor: It is optional but used here to explicitly define the primary constructor.
+ *
+ * Class Body: The {} for the class body is optional if there are no additional members.
+ * 
+ * Naming Convention:
+ * While not mandatory, it's common practice to define a class in a file named after the class. For example, the Person class would typically be stored in a file called Person.kt.
+ * This convention helps in easily locating the class code, as shown in the IntelliJ IDE.
+ */
+
+fun main() {
+
+
 }
