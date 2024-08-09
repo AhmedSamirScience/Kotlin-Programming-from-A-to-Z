0ae3bebc6a03539e644ab165fbc4e0b5983cd74c
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-10 00:36:19 +0300

    (ch8-Kotlin-Classes/1.2-Private-Properties-In-Constructor) -> docs: Add KDoc documentation for private properties in Kotlin
    - Explain how to define and use private properties in Kotlin
    - Include example with the Person3 class demonstrating private constructor properties
    - Show instance creation and access limitations for private properties
    
    This documentation provides insights into using private properties in Kotlin classes, focusing on access control and usage within and outside the class.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * Private Properties
+ * In Kotlin, you can define a constructor property as private. This property:
+ *
+ * Requires a value when an instance is created.
+ * Is accessible within the class itself.
+ * Is not accessible from outside the class.
+ * Here’s an example using the Person3 class:
+ */
+class Person3(private val id: String,
+              val name: String = "",
+              val age: Int = 0) {
+}
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+/**
+ * In this example:
+ *
+ * The id property is private, so it must be provided when creating an instance, but it can't be accessed from outside the class.
+ * The name and age properties are public and read-only (val), and they are optional parameters with default values.
+ * When creating an instance of Person3, you must provide an id, but you can choose whether to provide values for name and age.
+ * Here’s a simple application demonstrating this:
+ */
+fun main() {
+
+    val p1 = Person3("id1", "John", 50)
+    println("${p1.name} is ${p1.age}")
+    // compile-time error: id is a private property
+    // println("${p1.id}")
+
+    /**
+     * Output:
+     * John is 50
+     */
+
 }
