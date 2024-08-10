daf9bf9b271f068fee631bca0db2157aecc0330d
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-08-10 15:22:18 +0300

    (ch9-Kotlin-Classes/1-Singleton-Objects-in-Kotlin) -> docs: Add KDoc and example for Kotlin singleton objects
    - Document the use of the `object` keyword to create singleton objects
    - Provide examples of singleton objects with `init` blocks, member properties, functions, and overridden `toString()`
    - Demonstrate usage with `MathsUtils` for utility functions and `Session` for a singleton property
    - Include example output to illustrate singleton behavior and access
    
    This documentation helps in understanding Kotlin's singleton pattern and its application in code.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,89 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+/**
+ * This file demonstrates the concept of singleton objects in Kotlin using the `object` keyword.
+ *
+ * A singleton object can have:
+ * - One or more `init` blocks.
+ * - Zero or more member functions.
+ * - Zero or more member properties.
+ * - Its own override of `toString()`.
+ *
+ * Note: A singleton object cannot have a constructor.
+ */
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+object MathsUtils {
+
+    // Public member properties
+    val ZERO: Int = 0
+    val MIN: Int = -100
+    val MAX: Int
+
+    // Initialization block
+    init {
+        MAX = 1000
+    }
+
+    // Member functions
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    fun isLessThanMax(value: Int): Boolean {
+        return value < MAX
+    }
+
+    // Override toString
+    override fun toString(): String {
+        return "MathsUtils(ZERO=$ZERO, MIN=$MIN, MAX=$MAX)"
     }
 }
+
+object Session {
+
+    // Public member property
+    val id: String = "session-12345"
+
+    // Override toString
+    override fun toString(): String {
+        return "Session(id=$id)"
+    }
+}
+
+fun main() {
+    // Accessing the singleton's properties and functions in MathsUtils
+    println("MathsUtils.ZERO: ${MathsUtils.ZERO}")
+    println("MathsUtils.MIN: ${MathsUtils.MIN}")
+    println("MathsUtils.MAX: ${MathsUtils.MAX}")
+
+    println("Add 10 + 20: ${MathsUtils.add(10, 20)}")
+    println("Subtract 30 - 15: ${MathsUtils.subtract(30, 15)}")
+    println("Is 500 less than MAX: ${MathsUtils.isLessThanMax(500)}")
+
+    // Output the toString() of MathsUtils
+    println(MathsUtils)
+
+    // Demonstrating the use of the Session singleton
+    println(Session)
+
+    // Assign the Session object to a val
+    val s: Session = Session
+
+    // Print the alias
+    println(s)
+}
+
+/*
+ * Output:
+ * MathsUtils.ZERO: 0
+ * MathsUtils.MIN: -100
+ * MathsUtils.MAX: 1000
+ * Add 10 + 20: 30
+ * Subtract 30 - 15: 15
+ * Is 500 less than MAX: true
+ * MathsUtils(ZERO=0, MIN=-100, MAX=1000)
+ * Session(id=session-12345)
+ * Session(id=session-12345)
+ */
